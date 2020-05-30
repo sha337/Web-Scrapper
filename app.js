69b22c54006238b50920ec859f9e56fe91565291
sha337 <shabbaralee@gmail.com>
2020-05-30 21:21:53 +0530

    added selectors for tjr

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const writeStream = fs.createWriteStream('empData.csv');
+const writeStream = fs.createWriteStream('tjrPost.csv');
 
 //Write headers
-writeStream.write(`Name,Department,Bio \n`);
+writeStream.write(`title,imageUrl,description,author \n`);
 
 //request to url from where data is to scrapped
-request('https://www.qandle.com/our-team.html', (err, response, html) => {
+request('https://tjrexpress.com/2020/02/23/wall-of-inequality/', (err, response, html) => {
     if(!err && response.statusCode == 200){
         //selects whole HTML of page and saves it in $
         const $ = cheerio.load(html);
-
+        
         //Data that is to be scrapped
-        let name = $('.profile_detail > p > strong');
-        let department = $('.profile_detail > p:nth-of-type(2)');
-        let description = $('.profile_detail_info > p > small');    
+        let title = $('.entry-title').text();
+        let imageUrl = $('.post-thumbnail > a > img').attr("src");
+        let description = $('<div>').append($('.entry-content > p:not(:last-of-type)').clone()).html();   
+        let author = $('.entry-content > p:last-of-type');
+        description = description.replace(/,/g,';');
+        // for(var i=0;i<name.length;i++){
+        //     let emp_name = (name[i].children[0].data);
+        //     let emp_dept = (department[i].children[0].data);
+        //     let emp_des = (description[i].children[0].data);
         
-        for(var i=0;i<name.length;i++){
-            let emp_name = (name[i].children[0].data);
-            let emp_dept = (department[i].children[0].data);
-            let emp_des = (description[i].children[0].data);
-        
-            //Write to CSV
-            writeStream.write(`${emp_name}, ${emp_dept}, ${emp_des} \n`);
-        }
+        //     //Write to CSV
+        //     writeStream.write(`${emp_name}, ${emp_dept}, ${emp_des} \n`);
+        // }
+        //Write to CSV
+        writeStream.write(`${title}, ${imageUrl}, ${description}, ${author} \n`);
         console.log("Scrapping Done"); 
     }
 
